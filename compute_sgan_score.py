60844a82a0ceca61845ce191823e9ac78e4a4f4e
Vishnu <vishnubk93@gmail.com>
2020-06-16 17:25:26 +1000

    Fixed SGAN scoring code

--- compute_sgan_score.py
+++ compute_sgan_score.py
@@ -1,41 +1,65 @@
 from keras.utils import to_categorical
 from sklearn.ensemble import StackingClassifier
 from keras.models import load_model
-import time, sys
+import time, sys, os, glob
 import numpy as np
-total_epochs=400
+import argparse, pickle
+
+class NotADirectoryError(Exception):
+    pass
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError("Directory path is not valid.")
+
+
+parser = argparse.ArgumentParser(description='Score PFD files based on SGAN Machine Learning Model')
+parser.add_argument('-i', '--input_path', help='Absolute path of Input directory', default="/fred/oz002/vishnu/sgan/sample_data/")
+parser.add_argument('-o', '--output', help='Output file name',  default="/fred/oz002/vishnu/sgan/sample_data/")
+parser.add_argument('-b', '--batch_size', help='No. of pfd files that will be read in one batch', default='1', type=int)
+args = parser.parse_args()
+path_to_data = args.input_path
+batch_size = args.batch_size
+output_path = args.output
+dir_path(path_to_data)
+
+pfd_files = sorted(glob.glob(path_to_data + '*.pfd'))
+basename_pfd_files = [os.path.basename(filename) for filename in pfd_files]
+
+
 
 labelled_samples = 30000
 unlabelled_samples = 29283
 attempt_no = 1
-freq_phase_results = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/freq_phase_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
-time_phase_results = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/time_phase_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
-dm_curve_results = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/dm_curve_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
-pulse_profile_results = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/pulse_profile_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
+freq_phase_model = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/freq_phase_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
+time_phase_model = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/time_phase_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
+dm_curve_model = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/dm_curve_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
+pulse_profile_model = load_model('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/pulse_profile_best_discriminator_model_labelled_%d_unlabelled_%d_trial_%d.h5'%(labelled_samples, unlabelled_samples,  attempt_no))
+
+logistic_model = pickle.load(open('/fred/oz002/vishnu/pulsar_candidate_demystifier/codes/semi_supervised_trained_models/logistic_regression_labelled_%d_trial_%d.pkl'%(labelled_samples, attempt_no), 'rb'))
+
+dm_curve_combined_array = [np.load(filename[:-4] + '_dm_curve.npy') for filename in pfd_files]
+pulse_profile_combined_array = [np.load(filename[:-4] + '_pulse_profile.npy') for filename in pfd_files]
+freq_phase_combined_array = [np.load(filename[:-4] + '_freq_phase.npy') for filename in pfd_files]
+time_phase_combined_array = [np.load(filename[:-4] + '_time_phase.npy') for filename in pfd_files]
 
 
-model = LogisticRegression()
+reshaped_time_phase = [np.reshape(f,(48,48,1)) for f in time_phase_combined_array]
+reshaped_freq_phase = [np.reshape(f,(48,48,1)) for f in freq_phase_combined_array]
+reshaped_pulse_profile = [np.reshape(f,(64,1)) for f in pulse_profile_combined_array]
+reshaped_dm_curve = [np.reshape(f,(60,1)) for f in dm_curve_combined_array]
 
-#now test it in test data
-dm_curve_test = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in dm_curve_test])
-freq_phase_test = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in freq_phase_test])
-time_phase_test = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in time_phase_test])
-pulse_profile_test = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in pulse_profile_test])
-print('Size of test set is %d'%(len(dm_curve_test)))
-for i in range(2):
-    X_with_class = dm_curve_test[dm_curve_label_test == i]
+dm_curve_data = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in reshaped_dm_curve])
+pulse_profile_data = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in reshaped_pulse_profile])
+freq_phase_data = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in reshaped_freq_phase])
+time_phase_data = np.array([np.interp(a, (a.min(), a.max()), (-1, +1)) for a in reshaped_time_phase])
 
-    print('Number of examples in test set with label %d is %d' %(i, len(X_with_class)))
-
-predictions_freq_phase = freq_phase_results.predict([freq_phase_test])
-predictions_time_phase = time_phase_results.predict([time_phase_test])
-predictions_dm_curve = dm_curve_results.predict([dm_curve_test])
-predictions_pulse_profile = pulse_profile_results.predict([pulse_profile_test])
-
-float_value_time_phase = np.amax(predictions_time_phase, 1)
-float_value_freq_phase = np.amax(predictions_freq_phase, 1)
-float_value_dm_curve = np.amax(predictions_dm_curve, 1)
-float_value_pulse_profile = np.amax(predictions_pulse_profile, 1)
+predictions_freq_phase = freq_phase_model.predict([freq_phase_data])
+predictions_time_phase = time_phase_model.predict([time_phase_data])
+predictions_dm_curve = dm_curve_model.predict([dm_curve_data])
+predictions_pulse_profile = pulse_profile_model.predict([pulse_profile_data])
 
 predictions_time_phase = np.rint(predictions_time_phase)
 predictions_time_phase = np.argmax(predictions_time_phase, axis=1)
@@ -54,41 +78,17 @@ predictions_freq_phase = np.rint(predictions_freq_phase)
 predictions_freq_phase = np.argmax(predictions_freq_phase, axis=1)
 predictions_freq_phase = np.reshape(predictions_freq_phase, len(predictions_freq_phase))
 
-float_value_stacked_predictions_test = np.stack((float_value_freq_phase, float_value_time_phase, float_value_dm_curve, float_value_pulse_profile), axis=1)
-stacked_predictions_test = np.stack((predictions_freq_phase, predictions_time_phase, predictions_dm_curve, predictions_pulse_profile), axis=1)
-stacked_predictions_test = np.reshape(stacked_predictions_test, (25408,4))
 
-classified_results = model.predict(stacked_predictions_test)
-float_value_classified_results = model.predict_proba(stacked_predictions_test)[:,1]
-t1 = time.time()
-total_n = (t1-t0)
-print('Time taken for the code to execute was %s seconds' %(total_n))
-classified_results = np.rint(float_value_classified_results)
-f_score = f1_score(freq_phase_label_test, classified_results, average='binary')
-precision = precision_score(freq_phase_label_test, classified_results, average='binary')
-recall = recall_score(freq_phase_label_test, classified_results, average='binary')
-accuracy = (freq_phase_label_test == classified_results).sum()/len(freq_phase_label_test)
-tn, fp, fn, tp = confusion_matrix(freq_phase_label_test, classified_results).ravel()
-specificity = tn/(tn + fp)
-gmean = math.sqrt(specificity * recall)
-fpr = fp/(tn + fp)
-print('Results with HTRU-S Lowlat Survey Supervised Learning: correct one')
-print('Accuracy:', accuracy, 'F Score:', f_score, 'Precision:', precision, 'Recall:', recall, 'False Positive Rate:', fpr, 'Specificity:', specificity, 'G-Mean:', gmean)
 
-class_names = ['Non-Pulsar','Pulsar']
 
-cnf_matrix = confusion_matrix(freq_phase_label_test, classified_results)
-plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Normalized Confusion Matrix')
-np.save('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/predictions/true_labels.npy', freq_phase_label_test)
-np.save('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/predictions/predicted_labels.npy', float_value_classified_results)
+stacked_predictions = np.stack((predictions_freq_phase, predictions_time_phase, predictions_dm_curve, predictions_pulse_profile), axis=1)
+stacked_predictions = np.reshape(stacked_predictions, (len(dm_curve_data),4))
+classified_results = logistic_model.predict(stacked_predictions)
 
-#
-#if not os.path.isfile('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/combined_score.csv'):
-#    with open('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/combined_score.csv', 'w') as f:
-#        f.write('Labelled Samples' + ',' + 'Accuracy' + ',' + 'F-Score' + ',' + 'Precision' + ',' + 'Recall' + ',' + 'FPR' + ',' + 'Specificity' + ',' + 'G-Mean' + ',' + 'Total Epochs' + '\n')
-#
-#with open('/fred/oz002/vishnu/sgan/semi_supervised_trained_models/combined_score.csv', 'a') as f:
-#        f.write(str(labelled_samples) + ',' + str(accuracy) + ',' + str(f_score) + ',' + str(precision) + ',' + str(recall) + ',' + str(fpr) + ',' + str(specificity) + ',' + \
-#     str(gmean) + ',' + str(total_epochs) + '\n')
+with open('sgan_ai_score.csv', 'w') as f:
+    f.write('Filename, SGAN_score' + '\n') 
+    for i in range(len(pfd_files)):
+        f.write(basename_pfd_files[i] + ',' + str(classified_results[i]) + '\n')
+
 
 
--- extract_pfd_features.py
+++ extract_pfd_features.py
@@ -3,44 +3,73 @@ sys.path.append('/home/psr/software/psrchive/install/lib/python2.7/site-packages
 sys.path.append('/home/psr')
 import numpy as np
 from ubc_AI.training import pfddata
-import subprocess
+import subprocess, argparse
 import fnmatch
-#pfd_files_new_batch_nonpulsars = glob.glob('/beegfs/vishnu/scripts/neural_network/train/new_batch_nonpulsars/*.pfd')
-#pfd_files_new_batch_nonpulsars = glob.glob('/fred/oz002/vishnu/neural_network/lowlat_cands/new_batch_nonpulsars/*.pfd')
-#unlabelled_data = glob.glob('/fred/oz002/vishnu/LOWLAT/candidate_plots/')
-pfd_files = glob.glob()
-#for mediafile in glob.iglob(os.path.join('/fred/oz002/vishnu/LOWLAT/candidate_plots/', "2012-*", "*.pfd")):
-matches = []
+
+class NotADirectoryError(Exception):
+    pass
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError("Directory path is not valid.")
+
+
+
+parser = argparse.ArgumentParser(description='Extract pfd features as numpy array files.')
+parser.add_argument('-i', '--input_path', help='Absolute path of Input directory', default="/fred/oz002/vishnu/sgan/sample_data/")
+parser.add_argument('-o', '--output', help='Output file name',  default="/fred/oz002/vishnu/sgan/sample_data/")
+parser.add_argument('-b', '--batch_size', help='No. of pfd files that will be read in one batch', default='1', type=int)
+args = parser.parse_args()
+path_to_data = args.input_path
+batch_size = args.batch_size
+output_path = args.output
+dir_path(path_to_data)
+
+pfd_files = sorted(glob.glob(path_to_data + '*.pfd'))
+basename_pfd_files = [os.path.basename(filename) for filename in pfd_files]
 # If you want to recursively get all candidates in subdirectories
 #for root, dirnames, filenames in os.walk('/fred/oz002/vishnu/LOWLAT/candidate_plots/'):
 #    for filename in fnmatch.filter(filenames, '*.pfd'):
 #        matches.append(os.path.join(root, filename)) 
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+def chunks(pfd_files, n):
+    """Yield n-sized chunks from list of pfd files."""
+    for i in range(0, len(pfd_files), n):
+        yield pfd_files[i:i + n]
 
 # Extract 4 features based on Zhu et.al 2014
-batch_number=0
-for value in chunks(matches,6000):
+if batch_size > 1:
+    batch_number=0
+    for value in chunks(pfd_files,batch_size):
 
-    batch_number+=1
-    # Initialise data objects from getdata class
-    data_object_new_batch_nonpulsars = [pfddata(f) for f in value]
+        batch_number+=1
+        # Initialise data objects from getdata class
+        data_object = [pfddata(filename) for filename in value]
+
+        time_phase_data = [filename.getdata(intervals=48) for filename in data_object]
+        freq_phase_data = [filename.getdata(subbands=48) for filename in data_object]
+        pulse_profile_data = [filename.getdata(phasebins=64) for filename in data_object]
+        dm_curve_data = [filename.getdata(DMbins=60) for filename in data_object]
+    
+        ###Save all features as numpy array files
+        np.save(output_path + 'time_phase_data_batch_%d.npy' %int(batch_number), time_phase_data)
+        np.save(output_path + 'freq_phase_data_batch_%d.npy'%int(batch_number), freq_phase_data)
+        np.save(output_path + 'pulse_profile_data_batch_%d.npy' %int(batch_number), pulse_profile_data)
+        np.save(output_path + 'dm_curve_data_batch_%d.npy' %int(batch_number), dm_curve_data)
 
 
-    #1 time vs phase plot
-    time_phase_plots_new_batch_nonpulsars = [f.getdata(intervals=48) for f in data_object_new_batch_nonpulsars]
+else:
 
-    #2 freq vs phase plot
-    freq_phase_plots_new_batch_nonpulsars = [f.getdata(subbands=48) for f in data_object_new_batch_nonpulsars]
-    #3 Pulse Profile
-    pulse_profile_new_batch_nonpulsars = [f.getdata(phasebins=64) for f in data_object_new_batch_nonpulsars]
-    #4 DM Curve
-    dm_curve_new_batch_nonpulsars = [f.getdata(DMbins=60) for f in data_object_new_batch_nonpulsars]
+    data_object = [pfddata(filename) for filename in pfd_files]
+
+    time_phase_data = [filename.getdata(intervals=48) for filename in data_object]
+    freq_phase_data = [filename.getdata(subbands=48) for filename in data_object]
+    pulse_profile_data = [filename.getdata(phasebins=64) for filename in data_object]
+    dm_curve_data = [filename.getdata(DMbins=60) for filename in data_object]
     ###Save all features as numpy array files
-    np.save('/fred/oz002/vishnu/neural_network/lowlat_cands/unlaballed_data/time_phase_data_unlabelled_batch_%d.npy' %int(batch_number), time_phase_plots_new_batch_nonpulsars)
-
-    np.save('freq_phase_data_new_batch_nonpulsars.npy', freq_phase_plots_new_batch_nonpulsars)
-    np.save('pulse_profile_data_new_batch_nonpulsars_batch_%d.npy' %int(batch_number), pulse_profile_new_batch_nonpulsars)
-    np.save('dm_curve_data_new_batch_nonpulsars_batch_%d.npy' %int(batch_number), dm_curve_new_batch_nonpulsars)
+    for i in range(len(pfd_files)):
+        np.save(output_path + basename_pfd_files[i][:-4] + '_time_phase.npy', time_phase_data[i])
+        np.save(output_path + basename_pfd_files[i][:-4] + '_freq_phase.npy', freq_phase_data[i])
+        np.save(output_path + basename_pfd_files[i][:-4] + '_pulse_profile.npy', pulse_profile_data[i])
+        np.save(output_path + basename_pfd_files[i][:-4] + '_dm_curve.npy', dm_curve_data[i])
